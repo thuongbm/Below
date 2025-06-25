30ecac022b32e94f8cfd1e5c5971cb44bb6bbe3c
thuong <buiminhthuong30122005@gmail.com>
2025-06-25 10:45:40 +0700

    Add amination moverment

--- Below/Assets/Code/CharacterMove.cs
+++ Below/Assets/Code/CharacterMove.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CharacterMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    
+    
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -13,4 +16,6 @@ public class CharacterMove : MonoBehaviour
     {
         
     }
+
+    
 }
--- Below/Assets/Code/PlayerMoverment.cs
+++ Below/Assets/Code/PlayerMoverment.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 public class PlayerMoverment : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public float moveSpeed = 5f;
+    public float jump = 10f;
+    
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+    
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+    
+    void FixedUpdate()
+    {
+        rb2d.linearVelocity = moveSpeed * movement;
     }
 }
