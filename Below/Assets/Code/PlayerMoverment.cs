4bd39b4079f752d525ae5d244f1347c5b340a01a
thuong <buiminhthuong30122005@gmail.com>
2025-07-02 14:56:45 +0700

    Move

--- Below/Assets/Code/PlayerMoverment.cs
+++ Below/Assets/Code/PlayerMoverment.cs
@@ -5,7 +5,7 @@ public class PlayerMoverment : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     public float moveSpeed = 5f;
-    public float jump = 10f;
+    public float jump = 50f;
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
@@ -13,19 +13,23 @@ public class PlayerMoverment : MonoBehaviour
     private Rigidbody2D rb2d;
     private Vector2 movement;
     private bool isGrounded;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float input = Input.GetAxisRaw("Horizontal");
         movement.Normalize();
+        MoveAnimation(input);
     }
     
     void FixedUpdate()
@@ -37,6 +41,24 @@ public class PlayerMoverment : MonoBehaviour
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && isGrounded)
         {
             rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jump);
+        }
+    }
+
+    void MoveAnimation(float input)
+    {
+        if (input > 0)
+        {
+            animator.SetBool("IsRunning", true);
+            spriteRenderer.flipX = false;
+        }
+        else if (input < 0)
+        {
+            animator.SetBool("IsRunning", true);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
         }
     }
 }
